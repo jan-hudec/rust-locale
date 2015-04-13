bdb089cd42c5d7752bdada7a983723d176813a30
Jan Hudec <bulb@ucw.cz>
2015-04-13 19:02:01 +0200

    Silence warning
    Compiler now complains when deriving Copy and Clone on something containing
    raw pointers. The C structures are not supposed to be copyable as far as
    I can tell, so just remove the specification.

--- src/linux/ffi.rs
+++ src/linux/ffi.rs
@@ -52,7 +52,6 @@ pub const LC_ALL_MASK: ::libc::c_int = LC_CTYPE_MASK
 				 | LC_IDENTIFICATION_MASK;
 
 #[repr(C)]
-#[derive(Copy, Clone)]
 pub struct Struct_lconv {
     pub decimal_point: *mut ::libc::c_char,
     pub thousands_sep: *mut ::libc::c_char,
@@ -84,7 +83,6 @@ impl ::std::default::Default for Struct_lconv {
 }
 pub enum Struct___locale_data { }
 #[repr(C)]
-#[derive(Copy, Clone)]
 pub struct Struct___locale_struct {
     pub __locales: [*mut Struct___locale_data; 13usize],
     pub __ctype_b: *const ::libc::c_ushort,
